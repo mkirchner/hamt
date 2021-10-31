776672478adc9dcd0976cb2be1642ae1e5156c76
Marc Kirchner <mail@marc-kirchner.de>
2021-10-31 21:07:22 +0100

    Cleanly wrap word resources for testing

---
+++ test/words.h
@@ -0,0 +1,19 @@
+#ifndef __HAMT_TEST_WORDS_H__
+#define __HAMT_TEST_WORDS_H__
+
+#include <stdlib.h>
+
+/*
+ * Utility constants and functions for the english word list in
+ * text/words.txt.
+ */
+
+extern const size_t WORDS_MAX;
+
+void words_load(char ***words, size_t n_words);
+void words_free(char **words, size_t n);
+/* Shuffle words randomly; this creates weak refs to the existing words */
+char **words_create_shuffled_refs(char **words, size_t n_words);
+void words_free_refs(char **word_refs);
+
+#endif
